8472158b5d18852a129028b51b63ab1ba0ae27f8
kalpakyawalkarcse <kalpak.yawalkar.cse@gmail.com>
2026-09-11 11:00:30 +0530

    Added feature : Mark topics as done

--- main.py
+++ main.py
@@ -51,15 +51,39 @@ def view_topics():
         num = num + 1   
 
 
+def mark_as_studied():
+    print("\n---- Mark a Topic as Studied ----")
+    topics = load_topics()
+    if len(topics) == 0:
+        print("No topics saved yet.")
+        return
+    number  = 1 
+    for topic in topics:
+        print(number, "-", topic["subject"], ":", topic["topic_name"])
+        number = number + 1
+    choice = int(input("\nEnter the topic number you studied: "))
+    if choice < 1 or choice > len(topics):
+        print("Invalid topic number.")
+        return
+    today = str(date.today())
+    selected_topic = topics[choice - 1]
+    selected_topic["last_Studied"] = today  
+    save_topics(topics)
+    print("\nTopic marked as studied successfully!")
+
+
 def main() :
     print("Welcome to Study Priority Sorter!")
     print("1. Add a new topic")
     print("2. View all topics")
-    choice = input("Enter your choice (1 or 2): ")
+    print("3. Mark a topic as studied")
+    choice = input("Enter your choice (1, 2, or 3): ")
     if choice == "1":
         add_topic()
     elif choice == "2":
         view_topics()
+    elif choice == "3":
+        mark_as_studied()
     else:
         print("Invalid choice. Please try again.")
 
